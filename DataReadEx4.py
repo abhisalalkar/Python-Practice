29de12034bf8d9686e578474aa8a984d1c042f96
Abhi <abhijeetsalalkar122@gmail.com>
2026-08-20 10:27:23 +0530

    mul

--- DataReadEx4.py
+++ DataReadEx4.py
@@ -1,7 +1,7 @@
 #Program Reading two numerical value and multiply
 #DataReadEx4.py
-a=input("Enter First Number")
-b=input("Enter Second Number")
+a=input("Enter First value")
+b=input("Enter Second value")
 #convert numerical str values into int/float
 x=float(a)
 y=float(b)
