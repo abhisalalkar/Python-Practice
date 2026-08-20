c428cd13abb7806bcdf9b862ac0e512f9f6c0ab8
Abhi <abhijeetsalalkar122@gmail.com>
2026-08-20 10:59:07 +0530

    con hours into mins and sec

---
+++ DataReadEx10.py
@@ -0,0 +1,10 @@
+#Program for converting Hour into min and sec
+h=float(input("Enter Hours:"))
+min=h*60
+sec=min*60
+#sec=h*60*60
+print("*"*90)
+print("Hours:{}".format(h))
+print("Minutes:{}".format(min))
+print("Seconds:{}".format(sec))
+print("*"*90)
