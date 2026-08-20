4bffa9c147b9992863109277b66e3ba1782e359c
Abhi <abhijeetsalalkar122@gmail.com>
2026-08-20 10:52:26 +0530

    cal area and per of circle

---
+++ DataReadEx9.py
@@ -0,0 +1,12 @@
+#Program calculate area and perimeter of circle
+r=float(input("Enter the radius:"))
+#cal area
+ac=3.14*r**2
+#3.14*r*r
+#cal per of circle
+pc=2*3.14*r
+print("*"*80)
+print("Radius r:{}".format(r))
+print("Aria of circle={}".format(ac))
+print("Perimeter of circle={}".format(pc))
+print("*"*80)
