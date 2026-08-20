844c6d57e8cc4d938cf213293c82dd0ffcd28d08
Abhi <abhijeetsalalkar122@gmail.com>
2026-08-20 10:12:30 +0530

    multiply

---
+++ DataReadEx4.py
@@ -0,0 +1,13 @@
+#Program Reading two numerical value and multiply
+#DataReadEx4.py
+a=input("Enter First Value of a:")
+b=input("Enter Second Value of b:")
+#convert numerical str values into int/float
+x=float(a)
+y=float(b)
+z=x*y
+print("-"*50)
+print("First Value =",x)
+print("Second Value =",y)
+print("Product =",z)
+print("-"*50)
