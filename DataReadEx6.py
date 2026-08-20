e6569e656746b9eb8c98b1af171f16a89a62c143
Abhi <abhijeetsalalkar122@gmail.com>
2026-08-20 10:34:17 +0530

    multiply in print function

---
+++ DataReadEx6.py
@@ -0,0 +1,8 @@
+#Reading two numerical value and multiply them
+a=float(input("Enter value of a:"))
+b=float(input("Enter value of b:"))
+print("#"*70)
+print("First value =",a)
+print("Second value =",b)
+print("Product =",a*b)
+print("#"*70)
